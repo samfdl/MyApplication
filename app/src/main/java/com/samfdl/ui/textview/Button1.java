698b4c99f36fb26d78bf77ce8e0577b7d4d9b422
samfdl <samfdl@163.com>
2016-09-03 22:38:09 +0800

    修改button

--- app/src/main/java/com/samfdl/ui/textview/Button1.java
+++ app/src/main/java/com/samfdl/ui/textview/Button1.java
@@ -2,6 +2,9 @@ package com.samfdl.ui.textview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.samfdl.R;
 
@@ -10,5 +13,26 @@ public class Button1 extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ui_textview_button);
+        // 获取应用程序中的bn按钮
+        Button bn = (Button) findViewById(R.id.bn);
+        // 为按钮绑定事件监听器
+        bn.setOnClickListener(new MyClickListener()); // ①
+    }
+
+    // 定义一个单击事件的监听器
+    class MyClickListener implements View.OnClickListener {
+        // 实现监听器类必须实现的方法，该方法将会作为事件处理器
+        @Override
+        public void onClick(View v) {
+            TextView txt = (TextView) findViewById(R.id.txt);
+            txt.setText("普通按钮被单击了！");
+        }
+    }
+
+    // 定义一个事件处理方法
+    // 其中source参数代表事件源
+    public void clickHandler(View source) {
+        TextView txt = (TextView) findViewById(R.id.txt);
+        txt.setText("带文字的图片按钮被单击了");
     }
 }
