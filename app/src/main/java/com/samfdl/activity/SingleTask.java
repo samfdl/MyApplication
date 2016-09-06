69bafcb681877301d29dd49241050d2a95a15a18
samfdl <samfdl@163.com>
2016-09-06 19:10:47 +0800

    添加singletop 模式

---
+++ app/src/main/java/com/samfdl/activity/SingleTask.java
@@ -0,0 +1,14 @@
+package com.samfdl.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.samfdl.R;
+
+public class SingleTask extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_ball);
+    }
+}
