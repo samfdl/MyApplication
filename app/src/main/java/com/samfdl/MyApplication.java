d944b04eff990388b31ccffccac54e9599368360
samfdl <samfdl@163.com>
2018-05-14 17:22:57 +0800

    适配Android studio 新版本

--- app/src/main/java/com/samfdl/MyApplication.java
+++ app/src/main/java/com/samfdl/MyApplication.java
@@ -4,8 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.iflytek.cloud.SpeechUtility;
-import com.umeng.message.IUmengRegisterCallback;
-import com.umeng.message.PushAgent;
+//import com.umeng.message.IUmengRegisterCallback;
+//import com.umeng.message.PushAgent;
 
 /**
  * Created by samfdl on 2017/4/3.
@@ -16,19 +16,19 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Log.d("mytoken", "ddd");
-        PushAgent mPushAgent = PushAgent.getInstance(this);
-        //注册推送服务，每次调用register方法都会回调该接口
-        mPushAgent.register(new IUmengRegisterCallback() {
-            @Override
-            public void onSuccess(String deviceToken) {
-                //注册成功会返回device token
-                Log.d("mytoken", deviceToken);
-            }
-
-            @Override
-            public void onFailure(String s, String s1) {
-            }
-        });
+//        PushAgent mPushAgent = PushAgent.getInstance(this);
+//        //注册推送服务，每次调用register方法都会回调该接口
+//        mPushAgent.register(new IUmengRegisterCallback() {
+//            @Override
+//            public void onSuccess(String deviceToken) {
+//                //注册成功会返回device token
+//                Log.d("mytoken", deviceToken);
+//            }
+//
+//            @Override
+//            public void onFailure(String s, String s1) {
+//            }
+//        });
 
         SpeechUtility.createUtility(this, "appid=58eb1bc6");
     }
